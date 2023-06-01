b1f187708ade50dce60e6569719c1f77ca7f1678
alina <106563068+charcoalyy@users.noreply.github.com>
2023-06-01 19:57:22 -0400

    update about

--- portf/src/pages-general/About.js
+++ portf/src/pages-general/About.js
@@ -6,8 +6,8 @@ const About = () => {
             <div className="about-section" id="about-para">
                 <p>First year engineering student at McMaster University planning to specialize in software next fall.</p> 
                 <p>Lover of people-first design, problem-solving, and aesthetics, working to change the world by making our quotidian just a little easier.</p>
-                <p>Currently McMaster EcoCAR’s web and graphic designer & HOSA’s national technology analyst. Seeking opportunities to immerse myself in designing and/or developing products.</p>
-                <p id="above-about">In my free time: swimmer, creative writer, artist, piano player, and <a href="https://open.spotify.com/playlist/4TzNj3RTeH7S7D8t6FxiNW?si=a56f6789299c40ba" target="_blank">KR&B playlist curator</a>.</p>
+                <p>Currently a software developer @ FYELABS & national technology analyst @ HOSA Canada. Seeking more opportunities to immerse myself in designing and/or developing products.</p>
+                <p id="above-about">In my free time: swimmer, creative writer, artist, anime devotee, and <a href="https://open.spotify.com/playlist/4TzNj3RTeH7S7D8t6FxiNW?si=a56f6789299c40ba" target="_blank">KR&B playlist curator</a>. Message me about Hunter × Hunter <3</p>
                 <BackToHome about={true} />
             </div>
         </section>

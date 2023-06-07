1a12051b6a5898a5ef3d20f387a0acaae198872b
Alina Zeng <alina.yzeng@gmail.com>
2023-06-06 20:15:38 -0400

    figure touch-up

--- portf/src/bits-general/static/Header/index.js
+++ portf/src/bits-general/static/Header/index.js
@@ -3,10 +3,10 @@ import { Typography } from '@mui/material'
 export default ({ setGoDown }) => {
   return (
     <div className="header">
-      <div className="introduction">
-        <img src={require('figures/scrimbbly.gif')} alt="scribbly head" />
+      <div className="introduction" onClick={() => setGoDown(true)}>
+        <img src={require('figures/scwibbly.gif')} alt="scribbly head" className='header-image' />
         <Typography variant="h1" sx={{ color: 'white', textTransform: 'lowercase' }}>alina</Typography>
-        <Typography variant="h3" sx={{ color: 'white', cursor: 'pointer', textDecoration: 'underline' }} onClick={() => setGoDown(true)}>i like pretty things that work.</Typography>
+        <Typography variant="h3" sx={{ color: 'white', cursor: 'pointer', textDecoration: 'underline', mt: 0.5 }}>i like pretty things that work.</Typography>
       </div>
     </div>
   )

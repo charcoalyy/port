1135581f2fe574cb33173a6a7e92974f7760b285
alina 颖思 <106563068+charcoalyy@users.noreply.github.com>
2023-09-28 23:33:16 -0400

    redirect update

--- portf/src/bits-general/static/Header/index.js
+++ portf/src/bits-general/static/Header/index.js
@@ -6,8 +6,8 @@ export default ({ setGoDown }) => {
       <div className="introduction" onClick={() => setGoDown(true)}>
         <img src={require('figures/scwibbly.gif')} alt="scribbly head" className='header-image' />
         <Typography variant="h1" sx={{ color: 'white', textTransform: 'lowercase' }}>alina</Typography>
-        <Typography variant="h3" sx={{ color: 'white', cursor: 'pointer', textDecoration: 'underline', mt: 0.5 }}>i like pretty things that work.</Typography>
-    <a href="https://alinazeng.onrender.com/" style={{color: 'white'}}> this website is out of date! click here to go to my latest one</a>
+        // <Typography variant="h3" sx={{ color: 'white', cursor: 'pointer', textDecoration: 'underline', mt: 0.5 }}>i like pretty things that work.</Typography>
+        <a href="https://alinazeng.onrender.com/" style={{color: 'white'}}> this website is out of date! click here to go to my latest one</a>
       </div>
     </div>
   )
